a1674939195e9a773fdc7ad7750b483678573ac5
nigredon1991 <nigredon1991@gmail.com>
2017-04-02 10:51:18 +0500

    Adagrad worked

--- NN/Adagrad.py
+++ NN/Adagrad.py
@@ -4,8 +4,9 @@ import pandas
 
 coef_dic = 1.0  # coefficient dicrement weights in all steps
 coef_lear = 0.7 #coef_learning
-coef_A = 1.0   # coefficient for Adagrad 
+coef_A = 0.95   # coefficient for Adagrad 
 alfa = 1/10.0
+
 
 
 def sigmoid(x, alfa = 1/10.0, deriv = False):
@@ -102,37 +103,36 @@ class NN:
       
         self.delta_out = -(y - self.train_values_out)
         self.E_out = sum(self.delta_out*self.delta_out/2)
-        #self.delta_out = self.delta_out  * self.train_values_out* ( 1.0 - self.train_values_out)
         self.delta_out = self.delta_out  * sigmoid(self.train_values_out, deriv = True)
-
+#        print self.E_out
         self.delta_hidden[-1] = np.dot(self.delta_out,self.weights_out.T)
-#        self.delta_hidden[-1] = self.delta_hidden[-1]  * self.train_values_h[-1] * ( 1.0- self.train_values_h[-1])
         self.delta_hidden[-1] = self.delta_hidden[-1]  * sigmoid(self.train_values_h[-1], deriv = True)
         for j in range(len(self.size_hidden)-2 ,-1 ,-1):
             self.delta_hidden[j] = np.dot(self.delta_hidden[j+1],self.weights[j+1].T)
- #         self.delta_hidden[j] = self.delta_hidden[j]  * self.train_values_h[j] * ( 1.0- self.train_values_h[j])
             self.delta_hidden[j] = self.delta_hidden[j]  * sigmoid(self.train_values_h[j], deriv = True)
-        self.inc_c_out = np.dot(self.train_values_h[-1][:,None],self.delta_out[:,None].T)
-        self.inc_w_out = self.inc_w_out * coef_A + (self.inc_c_out)**2
-        self.weights_out = self.weights_out*coef_dic - coef * self.inc_w_out * self.inc_c_out/np.sqrt(self.inc_w_out+0.00000001)
         
+        self.inc_c_out = np.dot(self.train_values_h[-1][:,None],self.delta_out[:,None].T)        
+        self.inc_w_out = self.inc_w_out * coef_A + (1.0 - coef_A)*(self.inc_c_out)**2
+        self.weights_out = self.weights_out*coef_dic - coef * self.inc_c_out/( 1e-6 + np.sqrt(self.inc_w_out))
+
         if (len(self.size_hidden)>1):
             for i in range(len(self.size_hidden)-1,0,-1):
                 self.inc_c[i] = np.dot(self.train_values_h[i-1][:,None],self.delta_hidden[i][:,None].T)
-                self.inc_w[i] = self.inc_w[i] * coef_A + (self.inc_c[i])**2
-                self.weights[i] = self.weights[i]*1.0 - coef * self.inc_c[i] / np.sqrt(self.inc_w[i]+ 0.00000001)
+                self.inc_w[i] = self.inc_w[i] * coef_A + (1.0 - coef_A)*(self.inc_c[i])**2
+                self.weights[i] = self.weights[i]*coef_dic - coef * self.inc_c[i] / (1e-6 + np.sqrt(self.inc_w[i]))
+     
         self.inc_c[0] = np.dot(X[:,None],self.delta_hidden[0][:,None].T)
-        self.inc_w[0] = self.inc_w[0] * coef_A +  (self.inc_c[0])**2
-        self.weights[0] = self.weights[0]*coef_dic - coef * self.inc_c[0] /np.sqrt(self.inc_w[0]+0.000000001)
+        self.inc_w[0] = self.inc_w[0] * coef_A + (1.0 - coef_A)*(self.inc_c[0])**2
+        self.weights[0] = self.weights[0]*coef_dic - coef * self.inc_c[0] /(1e-6 + np.sqrt(self.inc_w[0]))
 
-        return 1
+        return self.E_out
 
 data = pandas.read_csv('train.csv')
 y_train = data["label"] # 42000
 
 X_train = data.drop("label", axis = 1) # 42000 * 784
 X_train = X_train.as_matrix()
-X_train[X_train<100] = 0   #normalization
+#X_train[X_train<100] = 0   #normalization
 X_train[X_train>1] = 1
 #X_train = X_train/250.0
 from time import time
@@ -145,7 +145,7 @@ y_temp = np.zeros(10)
 
 for i in np.random.randint(42000, size = 30000):
     y_temp[y_train[i]] = 1
-    MyNN.learning(X = X_train[i],y = y_temp,coef =0.8)
+    MyNN.learning(X = X_train[i],y = y_temp,coef =0.7)
     y_temp[y_train[i]] = 0
 
 for i in np.random.randint(42000, size = 2000):
@@ -166,9 +166,9 @@ for i in np.random.randint(42000, size = 1000):
    if(np.argmax(MyNN.predict(X_train[i])) == y_train[i]):
     acc+=1
     num[y_train[i]]+=1
-   else:
+#   else:
 #        imgplot = plt.imsave( '%d' % i ,np.reshape(X_train[i],(28,28)))
-          acc+=0.0
+
 print "accurance:", acc/1000
 print num
 print "time"
